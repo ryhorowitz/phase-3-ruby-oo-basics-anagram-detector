0fcba6a8e5a45b837398f519ffcc99f7d4cb20e5
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-03-20 16:28:29 -0400

    commit tree 290be553ad52224a9dda50223200a77c7c2e0756

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize word
+    @word = word
+  end
+
+  def match array
+    result = []
+    word_array = @word.chars.sort
+    # iterate through array
+    # pp word_array
+    array.each do |w| 
+      if w.chars.sort == word_array then result << w end
+    end
+    # check the index's word and see if it contains the same letters as @word
+    # break @ word into array with #chars
+    # bread index word into array with #chars
+    # sort the chars in both arrays
+    # if equal then they are an anagram
+    result
+  end
+end
